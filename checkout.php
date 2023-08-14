265d60fb6b6b7bb26cebc25c77d661f472210b50
gpgermano <guibagermano@gmail.com>
2023-08-13 22:44:16 -0300

    Template Checkout finalizado

--- checkout.php
+++ checkout.php
@@ -4,96 +4,84 @@
 <!-- Checkout Section Begin -->
 <section class="checkout spad">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h6><span class="icon_tag_alt"></span> Have a coupon? <a href="#">Click here</a> to enter your code
-                </h6>
-            </div>
-        </div>
-        <div class="checkout__form">
-            <h4>Billing Details</h4>
+        <div class="checkout__form bg-light">
+            <h4>Confirmas Dados</h4>
             <form action="#">
                 <div class="row">
                     <div class="col-lg-8 col-md-6">
                         <div class="row">
                             <div class="col-lg-6">
                                 <div class="checkout__input">
-                                    <p>Fist Name<span>*</span></p>
-                                    <input type="text">
+                                    <p>Nome Completo<span>*</span></p>
+                                    <input type="text" name="nome" id="nome">
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="checkout__input">
-                                    <p>Last Name<span>*</span></p>
-                                    <input type="text">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="checkout__input">
-                            <p>Country<span>*</span></p>
-                            <input type="text">
-                        </div>
-                        <div class="checkout__input">
-                            <p>Address<span>*</span></p>
-                            <input type="text" placeholder="Street Address" class="checkout__input__add">
-                            <input type="text" placeholder="Apartment, suite, unite ect (optinal)">
-                        </div>
-                        <div class="checkout__input">
-                            <p>Town/City<span>*</span></p>
-                            <input type="text">
-                        </div>
-                        <div class="checkout__input">
-                            <p>Country/State<span>*</span></p>
-                            <input type="text">
-                        </div>
-                        <div class="checkout__input">
-                            <p>Postcode / ZIP<span>*</span></p>
-                            <input type="text">
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="checkout__input">
-                                    <p>Phone<span>*</span></p>
-                                    <input type="text">
+                                    <p>CPF<span>*</span></p>
+                                    <input type="text" name="cpf" id="cpf">
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="checkout__input">
                                     <p>Email<span>*</span></p>
-                                    <input type="text">
+                                    <input type="text" name="email" id="email">
                                 </div>
                             </div>
-                        </div>
-                        <div class="checkout__input__checkbox">
-                            <label for="acc">
-                                Create an account?
-                                <input type="checkbox" id="acc">
-                                <span class="checkmark"></span>
-                            </label>
-                        </div>
-                        <p>Create an account by entering the information below. If you are a returning customer
-                            please login at the top of the page</p>
-                        <div class="checkout__input">
-                            <p>Account Password<span>*</span></p>
-                            <input type="text">
-                        </div>
-                        <div class="checkout__input__checkbox">
-                            <label for="diff-acc">
-                                Ship to a different address?
-                                <input type="checkbox" id="diff-acc">
-                                <span class="checkmark"></span>
-                            </label>
-                        </div>
-                        <div class="checkout__input">
-                            <p>Order notes<span>*</span></p>
-                            <input type="text"
-                                placeholder="Notes about your order, e.g. special notes for delivery.">
+                            <div class="col-lg-6">
+                                <div class="checkout__input">
+                                    <p>Telefone<span>*</span></p>
+                                    <input type="text" name="telefone" id="telefone">
+                                </div>
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="checkout__input">
+                                    <p>Rua<span>*</span></p>
+                                    <input type="text" name="rua" id="rua">
+                                </div>
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="checkout__input">
+                                    <p>Número<span>*</span></p>
+                                    <input type="text" name="numero" id="numero">
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="checkout__input">
+                                    <p>Bairro<span>*</span></p>
+                                    <input type="text" name="bairro" id="bairro">
+                                </div>
+                            </div>
+                            <div class="col-lg-5">
+                                <div class="checkout__input">
+                                    <p>Complemento</p>
+                                    <input type="text" name="complemento" id="complemento">
+                                </div>
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="checkout__input">
+                                    <p>CEP<span>*</span></p>
+                                    <input type="text" name="cep" id="cep">
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="checkout__input">
+                                    <p>Cidade<span>*</span></p>
+                                    <input type="text" name="cidade" id="cidade">
+                                </div>
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="checkout__input">
+                                    <p>Estado<span>*</span></p>
+                                    <input type="text" name="estado" id="estado">
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6">
                         <div class="checkout__order">
-                            <h4>Your Order</h4>
-                            <div class="checkout__order__products">Products <span>Total</span></div>
+                            <h4>Sua Compra</h4>
+                            <div class="checkout__order__products">Produtos <span>Total</span></div>
                             <ul>
                                 <li>Vegetables Package <span>$75.99</span></li>
                                 <li>Fresh Vegetable <span>$151.99</span></li>
@@ -101,30 +89,7 @@
                             </ul>
                             <div class="checkout__order__subtotal">Subtotal <span>$750.99</span></div>
                             <div class="checkout__order__total">Total <span>$750.99</span></div>
-                            <div class="checkout__input__checkbox">
-                                <label for="acc-or">
-                                    Create an account?
-                                    <input type="checkbox" id="acc-or">
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adip elit, sed do eiusmod tempor incididunt
-                                ut labore et dolore magna aliqua.</p>
-                            <div class="checkout__input__checkbox">
-                                <label for="payment">
-                                    Check Payment
-                                    <input type="checkbox" id="payment">
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                            <div class="checkout__input__checkbox">
-                                <label for="paypal">
-                                    Paypal
-                                    <input type="checkbox" id="paypal">
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                            <button type="submit" class="site-btn">PLACE ORDER</button>
+                            <button type="submit" class="site-btn" data-toggle="modal" data-target="#modalPagamento">FiNALIZAR COMPRA</button>
                         </div>
                     </div>
                 </div>
@@ -133,5 +98,26 @@
     </div>
 </section>
 <!-- Checkout Section End -->
+
+<!-- Modal -->
+<div class="modal fade" id="modalPagamento" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal Pagamento</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include 'rodape.php';?>
