6edfa59a50e6bbfb7004b88a9081db5b8e71c11d
gpgermano <guibagermano@gmail.com>
2023-08-27 22:09:49 -0300

    ajuste autenticar

--- sistema/autenticar.php
+++ sistema/autenticar.php
@@ -1,5 +1,7 @@
 <?php 
     include 'conexao.php';
+    @session_start(); // ignora versões em php
+
     
     $email_login = $_POST['email_login'];
     $senha_login = md5($_POST['senha_login']);
@@ -7,7 +9,20 @@
     $result =  $pdo->query("SELECT * from usuarios where email = '$email_login' and senha_crip = '$senha_login'");
     $dados = $result->fetchAll(PDO::FETCH_ASSOC);
     if (count($dados) > 0) {
-        echo 'Usuario Encontrado';
+        $_SESSION['nome_usuario'] = $dados[0]['nome'];
+        $_SESSION['email_usuario'] = $dados[0]['email'];
+        $_SESSION['cpf_usuario'] = $dados[0]['cpf'];
+        $_SESSION['nivel_usuario'] = $dados[0]['nivel'];
+
+        if ($_SESSION['nivel_usuario'] == 'Admin') {
+            echo '<h1>Bem vindo administrador</h1>';
+        }
+
+        
+        if ($_SESSION['nivel_usuario'] == 'Cliente') {
+            echo '<h1>Bem vindo Cliente</h1>';
+        }
+
     }
 
 
